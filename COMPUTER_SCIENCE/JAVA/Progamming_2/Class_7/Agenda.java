fa0ec1718e0c1a74f6908421e944a16fe4402152
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-02-01 22:41:44 -0300

    .

---
+++ COMPUTER_SCIENCE/JAVA/Progamming_2/Class_7/Agenda.java
@@ -0,0 +1,58 @@
+package COMPUTER_SCIENCE.JAVA.Progamming_2.Class_7;
+
+import java.util.ArrayList;
+
+public class Agenda {
+
+    private ArrayList<Contato> contatos;
+
+    public Agenda() {
+        this.contatos = new ArrayList<>();
+    }
+
+    public void inserirContato(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public boolean removerContato(String nome) {
+        Contato localizado = pesquisarContato(nome);
+        if (localizado != null) {
+            contatos.remove(localizado);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Contato pesquisarContato(String nome) {
+        Contato localizado = null;
+        for (Contato c : contatos) {
+            if(c.getNome().equals(nome)){
+                localizado = c;
+                break;
+            }
+        }
+        return localizado;
+    }
+
+    public boolean alterarTelefoneContato(String nome, String novoTelefone) {
+        Contato localizado = pesquisarContato(nome);
+        if (localizado != null) {
+            contatos.remove(localizado);
+            return true;
+        }
+        return false;
+    }
+
+    //Usado para testar os valores do seu objeto (toString)
+    @Override
+    public String toString() {
+        String temp = "Contatos: ";
+        int i = 0;
+        for (Contato c : contatos) {
+            i++;
+            temp += "\n" + i + " - " + c.getNome() + " - " + c.getTelefone();
+        }
+        return temp;
+    }
+}
