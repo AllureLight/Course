3da5ee417bf8367d455035b5460eb841a76a9e6e
Guilherme Andrade Nascimento Medeiros <guilherme.anm10@gmail.com>
2026-02-05 14:12:43 -0300

    .

---
+++ COMPUTER_SCIENCE/JAVA/Progamming_2/Exercise_4/TestaCaixa.java
@@ -0,0 +1,13 @@
+package COMPUTER_SCIENCE.JAVA.Progamming_2.Exercise_4;
+
+public class TestaCaixa {
+    public static void main(String[] args){
+        Caixa<String> caixaString = new Caixa<>();
+        caixaString.guardar("Programação Genérica");
+        System.out.println("Conteúdo da caixa de String: " + caixaString.pegar());
+
+        Caixa<Integer> caixaInteger = new Caixa<>();
+        caixaInteger.guardar(42);
+        System.out.println("Conteúdo da caixa de Integer: " + caixaInteger.pegar());
+    }
+}
